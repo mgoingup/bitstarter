f8521fa3edab13e056d45385ec97b2c2a8be74ff
Marco <mgoingup@gmail.com>
2015-07-02 05:28:30 +0000

    Configured index.js to use index.html

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var app = express();
+var fs = require('fs')
+
+buf = new Buffer(fs.readFileSync('index.html'))
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  response.send(buf.toString('utf8'))
 })
 
 app.listen(app.get('port'), function() {
